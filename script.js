6ac666477a9bbb867e3184f08707a806f77f842c
Nicolò Santilio <n.santilio97@gmail.com>
2020-12-05 16:15:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ var name = url.substring(url.indexOf("=") + 1);
 var parent = document.getElementById("myDiv");
 var input = document.createElement("input");
 input.value = name;
-parent.appendChild(input)
+parent.appendChild(input);
