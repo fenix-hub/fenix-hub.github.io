5540294716ad87f13c537f597a632224860821da
Nicolò Santilio <n.santilio97@gmail.com>
2020-12-05 16:08:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var url = "localhost:8000/something?name=ABC"
+var name = url.substring(url.indexOf("=") + 1);  
+
+var parent = document.getElementById("myDiv");
+var input = document.createElement("input");
+input.value = name;
+parent.appendChild(input)
